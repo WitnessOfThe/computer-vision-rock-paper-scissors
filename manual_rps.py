cf7996985d86df50abb293731a50dfa663d952a9
WitnessOfThe <puzurdan@gmail.com>
2023-02-09 21:46:12 +0000

    Implement_get_winner

--- manual_rps.py
+++ manual_rps.py
@@ -17,8 +17,38 @@ def get_user_choice():
   #      else:
 
    #         print('Try again, one from the list: "rock", "paper" or"scissors"')            
+def get_winner(computer_choice,user_choice):
+
+    if computer_choice == 'rock' and user_choice == 'scissors':
+
+        print("You lost")
+
+    elif computer_choice == 'rock' and user_choice == 'paper':
+
+        print("You won")
+
+    elif computer_choice == 'scissors' and user_choice == 'paper':
+
+        print("You lost")
+
+    elif computer_choice == 'scissors' and user_choice == 'rock':
+
+        print("You won")
+
+    elif computer_choice == 'paper' and user_choice == 'rock':
+
+        print("You lost")
+
+    elif computer_choice == 'paper' and user_choice == 'scissors':
+
+        print("You won")
+
+    elif computer_choice == user_choice:
+
+        print("It is a tie!")
 
 if __name__ == '__main__':
 
-    print(get_computer_choice())
-    print(get_user_choice())    
+#    print(get_computer_choice())
+#    print(get_user_choice())   
+     get_winner(get_computer_choice(),get_user_choice()) 
