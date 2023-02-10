bf47c01a82073090f535edfdee66b7eee1905fb1
WitnessOfThe <puzurdan@gmail.com>
2023-02-10 12:51:03 +0000

    count_down_creation

--- camera_rps.py
+++ camera_rps.py
@@ -1,21 +1,35 @@
 import cv2
 from keras.models import load_model
 import numpy as np
-
+import time
 
 def get_prediction():
-    model = load_model('keras_model.h5')
-    cap = cv2.VideoCapture(0)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-    data[0] = normalized_image
-    prediction = model.predict(data)
-    cv2.imshow('frame', frame)
 
-    print(prediction)
-    return prediction
+    t1  = time.time()
+    i   = 1
+    while True:
+
+        t2 = time.time()
+
+        if round(t2 - t1) > 3:
+
+            model = load_model('keras_model.h5')
+            cap = cv2.VideoCapture(0)
+            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+            ret, frame = cap.read()
+            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+            image_np = np.array(resized_frame)
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            data[0] = normalized_image
+            prediction = model.predict(data)
+            cv2.imshow('frame', frame)
+            return prediction
+
+        elif round(t2-t1) == i:
+
+            i  += 1
+            print(round(t2-t1))
+
 if __name__ == '__main__':
-    type(get_prediction())
+    
+    print(get_prediction())
